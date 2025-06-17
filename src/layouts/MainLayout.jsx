a3d0a7b14b48948196db11a0fb04f7261bcb2e41
Your Name <aqibqureshi4328@gmail.com>
2025-06-17 20:13:46 +0500

    header fixed

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,10 +4,17 @@ import { Outlet } from "react-router-dom";
 
 const MainLayout = () => (
   <div className="min-h-screen flex flex-col">
-    <Header />
-    <main className="flex-grow">
+    {/* Fixed Header */}
+    <div className="fixed top-0 left-0 right-0 z-50">
+      <Header />
+    </div>
+
+    {/* Main Content with Top Padding to avoid overlap and Bottom Margin */}
+    <main className="flex-grow pt-[80px] pb-[30px]">
       <Outlet />
     </main>
+
+    {/* Footer */}
     <Footer />
   </div>
 );
