c4dc811364a0689187907d5cd98e844b7296c216
AQIB ZAFEER <145930357+aqibzafeer@users.noreply.github.com>
2025-07-19 11:58:42 -0700

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
       <CategoryGrid />      
       <FeatureSection />
       <BuySection/>
-      {/* <OurValues /> */}
+      <OurValues />
       <FeaturedProducts />
     </div>
   );
